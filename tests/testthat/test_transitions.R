fae9274c836095929a117b0b3ce7ab03095e4f77
Paul R. Hibbing <paulhibbing@gmail.com>
2019-02-19 23:15:03 -0500

    Set up testthat

---
+++ tests/testthat/test_transitions.R
@@ -0,0 +1,19 @@
+context("Transition Analyses")
+library(PAutilities)
+
+testthat::test_that("Transition analyses produce expected output", {
+  set.seed(8)
+  predictions <- sample(c(0,1), 100, TRUE, c(3, 1))
+  references  <- sample(c(0,1), 100, TRUE, c(4,1))
+
+  info <- get_transition_info(predictions, references, 10)
+
+  testthat::expect_equal_to_reference(
+    info,
+    "transition_info.rds"
+  )
+  testthat::expect_equal_to_reference(
+    summary(info),
+    "transition_summary.rds"
+  )
+})
