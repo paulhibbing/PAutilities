1614f1ccf9b851e7f912bae5ef7bb94b0773f526
Paul R. Hibbing <prhibbing@cmh.edu>
2021-01-08 13:06:22 -0600

    Revise get_BMI_percentile and draft code to classify severe obesity

--- R/get_BMI_percentile.R
+++ R/get_BMI_percentile.R
@@ -5,21 +5,27 @@
 #' @param age_yrs age in years
 #' @param age_mos age in months (optional)
 #' @param sex Character scalar indicating participant's sex
+#' @param BMI numeric. Body mass index
 #' @param output What should be returned: raw percentile, weight status
-#'   classification, or both?
+#'   classification, both percentile and classification, or a full summary (BMI,
+#'   percentile, classification, and severe obesity cutoff)?
 #'
 #' @return One of: A numeric scalar giving the BMI percentile (for \code{output
 #'   = "percentile"}); a factor scalar giving the weight status (for
-#'   \code{output = "classification"}); or a list with the percentile and
-#'   classification (for \code{output = "both"}).
+#'   \code{output = "classification"}); a list with the percentile and
+#'   classification (for \code{output = "both"}); or a list with the BMI,
+#'   percentile, classification, and severe obesity cutoff (for \code{output =
+#'   "summary"}).
 #' @export
 #'
-#' @details If \code{age_mos} is \emph{not} provided, it will be calculated
-#'   based on \code{age_yrs}, assuming 365.2425 days per year and 30.4375 days
-#'   per month. Depending on how the initial age calculation was made, rounding
-#'   error will occur. Thus, use of the \code{\link{get_age}} function is
-#'   recommended. If \code{age_mos} \emph{is} provided, \code{age_yrs} can be
-#'   passed as \code{NULL}.
+#' @details Only one of \code{age_mos} and \code{age_yrs} is required. The
+#'   former will be used if both are provided. If \code{age_mos} is \emph{not}
+#'   provided, it will be calculated based on \code{age_yrs}, assuming 365.2425
+#'   days per year and 30.4375 days per month. Depending on how the initial age
+#'   calculation was made, rounding error will occur. Thus, use of the
+#'   \code{\link{get_age}} function is recommended, with \code{units =
+#'   "months"}. If \code{BMI} is provided, there is no need to pass
+#'   \code{weight_kg} or \code{height_cm}.
 #'
 #' @references
 #' This function was developed with reference to public domain resources
@@ -30,69 +36,99 @@
 #'
 #' \url{https://www.cdc.gov/healthyweight/assessing/bmi/childrens_bmi/tool_for_schools.html}
 #'
+#' @seealso
+#'
+#' \href{https://www.ahajournals.org/doi/10.1161/CIR.0b013e3182a5cfb3}{Kelly et al. (2013)}
+#' \href{https://academic.oup.com/ajcn/article/90/5/1314/4598130}{Flegal et al. (2009)}
+#'
 #' @examples
-#' get_BMI_percentile(39.4, 144.5, 12.35, sex = "M")
+#' get_BMI_percentile(39.4, 144.5, 12.35, sex = "Male")
 get_BMI_percentile <- function(
-  weight_kg, height_cm, age_yrs,
-  age_mos = NULL, sex = c("M", "F"),
-  output = c("percentile", "classification", "both")
+  weight_kg, height_cm, age_yrs = NULL,
+  age_mos = NULL, sex = c("Male", "Female"), BMI = NULL,
+  output = c("percentile", "classification", "both", "summary")
 ) {
 
-  sex <- match.arg(sex, c("M", "F", "Error"), several.ok = FALSE)
-  stopifnot(sex %in% c("M", "F"))
+  ## Format variables
 
-  reference <- standards[standards$Sex == sex, ]
-  stopifnot(
-    !any(duplicated(reference$Age)),
-    all(diff(order(reference$Age)) == 1)
-  )
+    sex %<>% percentile_sex(.)
 
-  if (is.null(age_mos)) {
-    daysold <- age_yrs * 365.2425
-    age_mos <- daysold / 30.4375
-  }
+    reference <- percentile_reference(sex)
 
-  BMI <- weight_kg / (height_cm ^ 2) * 10000
+    age_mos %<>% percentile_age(age_yrs)
 
-  increment <- age_mos - floor(age_mos + 0.5) + 0.5
+    BMI %<>% percentile_BMI(weight_kg, height_cm)
 
-  greater_index <- max(
-    which(reference$Age <= (age_mos + 1))
-  )
+  ## Prepare for calculations
 
-  lesser_index <- max(
-    which(reference$Age <= (age_mos))
-  )
+    increment <-
+      floor(age_mos + 0.5) %>%
+      {age_mos - . + 0.5}
 
-  L <- (increment * reference$L[greater_index]) +
-    ((1-increment)* reference$L[lesser_index])
-  M <- (increment * reference$M[greater_index]) +
-    ((1-increment)* reference$M[lesser_index])
-  S <- (increment * reference$S[greater_index]) +
-    ((1-increment)* reference$S[lesser_index])
+    greater_index <- percentile_index(reference, age_mos + 1)
 
-  Z_score <- (((BMI/M)^L)-1)/(L*S)
+    lesser_index <- percentile_index(reference, age_mos)
 
-  percentile <- unname(
-    floor(stats::pnorm(Z_score) * 1000) / 10
-  )
-  classification <- cut(
-    percentile,
-    c(-Inf, 5, 85, 95, Inf),
-    c("Underweight", "Normal Weight", "Overweight", "Obese"),
-    right = FALSE
-  )
+  ## Get Z score and percentile
 
-  output <- match.arg(output)
-
-  switch(
-    output,
-    "percentile" = percentile,
-    "classification" = classification,
-    "both" = stats::setNames(
-      list(percentile, classification),
-      c("percentile", "classification")
+    info <- mapply(
+      percentile_lms,
+      colname = c("L", "M", "S"),
+      MoreArgs = list(
+        reference = reference,
+        lesser_index = lesser_index,
+        greater_index = greater_index,
+        increment = increment
+      ),
+      SIMPLIFY = FALSE
     )
-  )
+
+    z_score <-
+      c(info, BMI = list(BMI)) %>%
+      do.call(percentile_z, .)
+
+    percentile <-
+      {stats::pnorm(z_score) * 1000} %>%
+      floor(.) %>%
+      {. / 10} %>%
+      unname(.)
+
+    bmi_severe <-
+      do.call(percentile_back_calculate, info) %>%
+      {. * 1.2}
+
+  ## Get classification
+
+    classification <-
+      cut(
+        percentile,
+        c(-Inf, 5, 85, 95, Inf),
+        c("Underweight", "Normal Weight", "Overweight", "Obese"),
+        right = FALSE
+      ) %>%
+      as.character(.) %>%
+      ifelse(BMI >= bmi_severe, "Severe_Obese", .) %>%
+      factor(c(
+        "Underweight", "Normal Weight", "Overweight", "Obese", "Severe_Obese"
+      ))
+
+  ## Finish up
+
+    output <- match.arg(output)
+
+    switch(
+      output,
+      "percentile" = percentile,
+      "classification" = classification,
+      "both" = stats::setNames(
+        list(percentile, classification),
+        c("percentile", "classification")
+      ),
+      "summary" = stats::setNames(
+        list(BMI, percentile, classification, bmi_severe),
+        c("BMI", "percentile", "classification", "severe_obesity_bmi_cutoff")
+      ),
+      NULL
+    )
 
 }
---
+++ R/get_BMI_percentile_helpers.R
@@ -0,0 +1,131 @@
+#' Internal helper functions for \code{\link{get_BMI_percentile}}
+#'
+#' @inheritParams get_BMI_percentile
+#' @param reference reference subset for calculations
+#' @param colname character. Column name of \code{reference} on which to operate
+#' @param greater_index upper match index of \code{reference}
+#' @param lesser_index lower match index of \code{reference}
+#' @param increment proportional distance between
+#'   \code{reference[lesser_index, ]} and \code{reference[greater_index, ]}
+#' @param L lambda
+#' @param M mu
+#' @param S sigma
+#' @param p percentile for which to back-calculate a BMI (must be between 0 and
+#'   1)
+#'
+#' @seealso \href{https://pubmed.ncbi.nlm.nih.gov/24992748/}{Flegal and Cole (2013)}
+#'
+#' @name bmi_internal
+#' @keywords internal
+NULL
+
+#' @rdname bmi_internal
+#' @keywords internal
+percentile_age <- function(age_mos, age_yrs) {
+
+  if (is.null(age_mos)) {
+
+    if (is.null(age_yrs)) stop(
+      "`age_mos` is NULL, but `age_yrs` has not been provided"
+    )
+
+    age_mos <-
+      {age_yrs * 365.2425} %>% # Convert to age in days
+      {. / 30.4375} # Then to age in months
+
+  }
+
+  age_mos
+
+}
+
+#' @rdname bmi_internal
+#' @keywords internal
+percentile_BMI <- function(BMI, weight_kg, height_cm) {
+
+  if (is.null(BMI)) {
+
+    weight_kg / (height_cm ^ 2) * 10000
+
+  } else {
+
+    BMI
+
+  }
+
+}
+
+#' @rdname bmi_internal
+#' @keywords internal
+percentile_index <- function(reference, age_mos) {
+  {reference$Age <= age_mos} %>%
+  which(.) %>%
+  max(.)
+}
+
+#' @rdname bmi_internal
+#' @keywords internal
+percentile_lms <- function(
+  reference, colname, lesser_index, greater_index, increment
+) {
+
+  lesser_proportion <-
+    reference[lesser_index, colname] * (1 - increment)
+
+  greater_proportion <-
+    reference[greater_index, colname] * increment
+
+  lesser_proportion + greater_proportion
+
+}
+
+#' @rdname bmi_internal
+#' @keywords internal
+percentile_reference <- function(sex) {
+
+  {standards$Sex == sex} %>%
+  standards[., ] %T>%
+  {stopifnot(
+    !any(duplicated(.$Age)),
+    all(diff(order(.$Age)) == 1),
+    nrow(.) > 0
+  )}
+
+}
+
+#' @rdname bmi_internal
+#' @keywords internal
+percentile_sex <- function(sex = c("error", "male", "female")) {
+  # Handling of match.arg is pretty ugly here. Goal is to prevent a default sex
+  # from being silently selected, but I am sure that could be accomplished in a
+  # much more elegant way. I just don't have the patience to set it up right
+  # now.
+  sex <- tolower(sex)
+  sex <- match.arg(sex)
+  if (!sex %in% c("male", "female")) stop(
+    "Could not match sex to the available",
+    " options (\"Male\" or \"Female\")"
+  )
+  switch(sex, "male" = "M", "female" = "F") # To match format of standards$Sex
+}
+
+#' @rdname bmi_internal
+#' @keywords internal
+percentile_z <- function(BMI, L, M, S) {
+  {BMI/M} %>%
+  {.^L} %>%
+  {.-1} %>%
+  {./(L*S)}
+}
+
+#' @rdname bmi_internal
+#' @keywords internal
+percentile_back_calculate <- function(p = 0.95, L, M, S) {
+  p %T>%
+  {stopifnot(. >= 0, . <= 1)} %>%
+  stats::qnorm(.) %>%
+  {. * L * S} %>%
+  {. * (M ^ L)} %>%
+  {. + (M ^ L)} %>%
+  {. ^ (1/L)}
+}
