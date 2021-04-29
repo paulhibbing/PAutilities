3b88b9255198dbfd57c6e2f878edc72fd7792e59
Paul R. Hibbing <paulhibbing@gmail.com>
2021-04-29 10:31:05 -0500

    Start changing some terminology around cluster-based bout identification

--- R/bout_general_1Wrappers.R
+++ R/bout_general_1Wrappers.R
@@ -3,42 +3,22 @@
 #' @param x A vector of activity labels, e.g. minute-by-minute classifications
 #'   of \code{sedentary behavior}, \code{light physical activity}, or
 #'   \code{moderate-to-vigorous physical activity}
-#' @param method character. One of \code{sequential} (a traditional approach,
-#'   the default) or \code{targeted} (focusing on the class indicated by
-#'   \code{target})
+#' @param method character. The bout identification method to use. Currently,
+#'   only one option is supported (\code{cluster-based}), but others may be
+#'   added in the future
 #' @param longest_allowable_interruption numeric. The number of consecutive
 #'   out-of-class readings that must occur to satisfy the bout termination
 #'   criteria
-#' @param required_percent numeric (1-100). The minimum percentage of time
-#'   within the bout period that must be non-interrupted in order for the bout
-#'   to qualify as valid. See details.
+#' @param required_percent numeric (1-100). The minimum percentage of the full
+#'   bout period that must be spent engaged in the target behavior. Stated
+#'   differently, interruptions can compose no more than
+#'   (100-\code{required_percent})% of the bout.
 #' @param max_n_interruptions numeric. The maximum number of interruption events
 #'   that are allowed before a bout will be considered invalid.
 #' @param target character. The value of \code{x} for which bout information is
-#'   desired (applicable only when \code{method = "targeted"})
+#'   desired.
 #' @param target_buffer numeric. Maximum separation between runs of
 #'   \code{target}, beyond which they will not be clustered together
-#'
-#' @details When \code{method = "sequential"}, only one of the arguments
-#'   applies (\code{longest_allowable_interruption}). The other arguments
-#'   are for \code{method = "targeted"}. However, the output from both
-#'   settings will provide details about interruptions and percentage of
-#'   time spent in the behavior, so additional restrictions can be applied
-#'   by the user after running this function.
-#'
-#'   The sequential method runs much faster, and returns information about bouts
-#'   of all behaviors in \code{x}. The targeted method runs more slowly
-#'   (depending on how many state changes there are in \code{x}), and returns
-#'   information only about the class specified by the setting of \code{target}.
-#'
-#'   The sequential method defines bouts for each state of \code{x}, and each of
-#'   the states can register as an 'interruption' to the others. As such, the
-#'   sequential method may not produce the same frequencies as \code{table(x)}
-#'   when there are more than two possible classes, because within each bout all
-#'   of the 'interruption' classes are grouped together. In contrast, the
-#'   targeted method defines bouts only for the state identified by
-#'   \code{target}, and for that state, the output will reflect the same total
-#'   as \code{table(x)}.
 #'
 #' @note \code{x} must be an atomic vector. Factors should be cast to integer or
 #'   character prior to passing into this function.
@@ -53,17 +33,17 @@
 #' intensity <- as.character(get_intensity(ex_data$METs))
 #' get_bouts(intensity, "sequential", 5)
 #' \donttest{
-#' get_bouts(intensity, "targeted", 5, 50, 3, "MVPA", 30)
+#' get_bouts(intensity, "cluster-based", 5, 50, 3, "MVPA", 30)
 #' }
 get_bouts <- function(
-  x, method = c("sequential", "targeted"),
+  x, method = "cluster-based",
   longest_allowable_interruption = Inf, required_percent = 100,
   max_n_interruptions = Inf, target = NULL, target_buffer = NULL
 ) {
 
   method <- match.arg(method)
 
-  if (method == "targeted") {
+  if (method == "cluster-based") {
     check_targeted(x, target, required_percent, target_buffer)
     if (is.null(target_buffer)) target_buffer <- Inf
   }
@@ -73,7 +53,7 @@ get_bouts <- function(
     "sequential" = group_runs_sequential(
       x, longest_allowable_interruption
     ),
-    "targeted" = group_runs_targeted(
+    "cluster-based" = group_runs_targeted(
       x, target, required_percent,
       longest_allowable_interruption,
       max_n_interruptions, target_buffer
